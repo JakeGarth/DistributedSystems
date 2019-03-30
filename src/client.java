ccf1d8d647171f5d804775f58e1f96076242a506
Lewis <l3wis992@gmail.com>
2019-03-30 14:09:01 +1100

    Changes to be committed: 	new file:   bin/.gitignore 	modified:   bin/client.class 	modified:   bin/server.class 	modified:   src/client.java 	modified:   src/server.java

--- src/client.java
+++ src/client.java
@@ -1,9 +1,17 @@
+import java.io.*;
+import java.net.*;
 
 public class client {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			Socket s = new Socket("localhost", 6666);
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+			dout.writeUTF("Hello Server!123");
+			dout.flush();
+			dout.close();
+			s.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-
 }
--- src/server.java
+++ src/server.java
@@ -1,9 +1,17 @@
+import java.io.*;
+import java.net.*;
 
 public class server {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			ServerSocket ss = new ServerSocket(6666);
+			Socket s = ss.accept();// establishes connection
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			String str = (String) dis.readUTF();
+			System.out.println("message= \n" + str);
+			ss.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-
 }
